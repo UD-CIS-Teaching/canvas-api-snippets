39ba729c66e71c62eaa23db055b88461202ac75a
Austin Cory Bart <acbart@udel.edu>
2026-02-13 13:48:21 -0500

    Add script to download student emails as CSV
    This script fetches student data from a course API and downloads their emails as a CSV file.

---
+++ download_canvas_emails.ts
@@ -0,0 +1,65 @@
+(async function(){
+
+    // https://stackoverflow.com/questions/8735792/how-to-parse-link-header-from-github-api
+    let linkParser = (linkHeader)=>{
+        let re = /,[\s]*<(.*?)>;[\s]*rel="next"/g;
+        let result = re.exec(linkHeader);
+        if (result == null) {
+            return null;
+        }
+        return result[1];
+    }
+
+    function downloadBlob(content, filename, contentType) {
+      // Create a blob
+      var blob = new Blob([content], { type: contentType });
+      var url = URL.createObjectURL(blob);
+
+      // Create a link to download it
+      var pom = document.createElement('a');
+      pom.href = url;
+      pom.setAttribute('download', filename);
+      pom.click();
+    }
+
+    let course = ENV.context_asset_string;
+    let courseId = course.split("_")[1];
+    let base = ENV.DEEP_LINKING_POST_MESSAGE_ORIGIN;
+    let api = `${base}/api/v1`;
+    let courseUrl = `${api}/courses/${courseId}`;
+    let noApiCourseUrl = `${base}/courses/${courseId}`;
+
+
+    // Get all the students
+    async function getAllStudents() {
+        let all = [];
+        let parameters = {
+        };
+        let next = courseUrl+'/users/?per_page=100';
+        do {
+            let response = await fetch(next, parameters);
+            all.push(...await response.json());
+            let links = response.headers.get('link');
+            next = linkParser(links);
+        } while (next != null);
+        return await all; 
+    }
+
+    let students = await getAllStudents();
+    const filename = `emails_${courseId}.csv`;
+    /*let emails = students.map(async (student) => {
+        let details = await fetch(`${api}/users/${student.id}`, {'include[]': 'email'})
+        console.log("Finished", details.email);
+        return details.email;
+    })*/
+    let defaultHeaders = ['name', 'cid', 'sid', 'email'];
+    const csv = [defaultHeaders.join(","),
+                 ...students.map((user => {
+        return [user.sortable_name, user.id, user.login_id, user.email]
+            .map(String)  // convert every value to String
+            .map(v => v.replaceAll('"', '""'))  // escape double colons
+            .map(v => `"${v}"`)  // quote it
+            .join(',');  // comma-separated
+    }))].join("\n");
+    downloadBlob(csv, filename, "text/csv;charset=utf-8;")
+})()
